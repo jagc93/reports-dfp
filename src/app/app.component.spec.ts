162f2c9cefc28db2048e4d01ab8ecc56220a79a0
jagc93 <jagc93.jg@gmail.com>
2020-11-09 15:40:20 -0500

    Se acomoda el appComponent.ts apuntando directamente al router-outlet, se remueve el html y el scss

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'dfp-reports-page'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('dfp-reports-page');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('dfp-reports-page app is running!');
-  });
 });
